5bb33f3b5dfdb02f9f9e7a3813cd5285b53c7aa9
Julia <juhpassamani@hotmail.com>
2015-11-05 16:10:01 -0200

    Persistencia criada + teste para insercao de jogos

---
+++ src/br/edu/ifsul/testes/TesteInserirGame.java
@@ -0,0 +1,46 @@
+
+package br.edu.ifsul.testes;
+
+import br.edu.ifsul.modelo.Game;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author jupassamani
+ * @email juliapassamani@me.com
+ */
+public class TesteInserirGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        try {
+            emf = Persistence.createEntityManagerFactory("TrabalhoSegundaEtapa");
+            em = emf.createEntityManager();
+            Game g = new Game();
+            g.setAno("2000");
+            g.setNome("Resident Evil");
+            g.setJogabilidade("Multiplayer");
+            
+            em.getTransaction().begin();
+            em.persist(g); // inserir
+            em.getTransaction().commit();
+        } catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Erro: "+e.getMessage());
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().commit();
+            }
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+}
