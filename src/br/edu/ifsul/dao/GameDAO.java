250c52de5922e825265b66d5b69f80f78d00484f
Julia <juhpassamani@hotmail.com>
2015-11-12 10:32:24 -0200

    GameDAO criado

--- src/br/edu/ifsul/dao/GameDAO.java
+++ src/br/edu/ifsul/dao/GameDAO.java
@@ -6,11 +6,119 @@
 
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.modelo.Game;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author jupassamani
  * @email juliapassamani@me.com
  */
-public class GameDAO {
+public class GameDAO implements Serializable{
+    private List<Game> listarTodos;
 
+    public GameDAO() {
+    }
+
+    public void persistir(Game objeto) throws Exception{
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TrabalhoSegundaEtapaPU");
+        EntityManager em = emf.createEntityManager();
+        try {
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.persist(objeto);
+            em.getTransaction().commit();
+        } catch(Exception e){
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            throw new Exception("Erro na operação de persistência: "+e.getMessage());
+        } finally {
+            em.close();
+            emf.close();
+        }        
+    }
+    
+    public void merge(Game objeto) throws Exception{
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TrabalhoSegundaEtapaPU");
+        EntityManager em = emf.createEntityManager();
+        try {
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.merge(objeto);
+            em.getTransaction().commit();
+        } catch(Exception e){
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            throw new Exception("Erro na operação de persistência: "+e.getMessage());
+        } finally {
+            em.close();
+            emf.close();
+        }        
+    }    
+    
+    public void remove(Integer id) throws Exception{
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TrabalhoSegundaEtapaPU");
+        EntityManager em = emf.createEntityManager();
+        try {
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            Game objeto = em.find(Game.class, id);
+            em.remove(objeto);
+            em.getTransaction().commit();
+        } catch(Exception e){
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            throw new Exception("Erro na operação de persistência: "+e.getMessage());
+        } finally {
+            em.close();
+            emf.close();
+        }        
+    }   
+    
+    public Game getObjectById(Integer id) throws Exception{
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TrabalhoSegundaEtapaPU");
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Game.class, id);
+        } catch(Exception e){
+            if (em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            throw new Exception("Erro na operação de persistência: "+e.getMessage());
+        } finally {
+            em.close();
+            emf.close();
+        }        
+    }     
+
+    public List<Game> getListarTodos() throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TrabalhoSegundaEtapaPU");
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery("from Game order by nome").getResultList();
+        } catch(Exception e){
+            throw new Exception("Erro na operação de persistência: "+e.getMessage());
+        } finally {
+            em.close();
+            emf.close();
+        }    
+    }
+
+    public void setListarTodos(List<Game> listarTodos) {
+        this.listarTodos = listarTodos;
+    }
 }
